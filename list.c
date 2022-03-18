de9e1d8b62fad89121f0e712c17188c8c4548ffd
SEBASTIAN-ALES1 <>
2022-03-18 00:56:16 +0000

    new try

--- list.c
+++ list.c
@@ -55,7 +55,7 @@ void * lastList(List * list) {
 }
 
 void * prevList(List * list) {
-  if (list ==NULL || list->head ==NULL || list->current == NULL || list->current->next == NULL) return NULL;
+  //if (list ==NULL || list->head ==NULL || list->current == NULL || list->current->next == NULL) return NULL;
   list->current = list->current->prev;
   return list->current->data;
 }

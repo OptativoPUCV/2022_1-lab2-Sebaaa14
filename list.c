01ace23a21e4f67debd5263b9c9632024a84c8c4
SEBASTIAN-ALES1 <>
2022-03-18 01:02:36 +0000

    new try

--- list.c
+++ list.c
@@ -61,7 +61,7 @@ void * prevList(List * list) {
 }
 
 void pushFront(List * list, void * data) {
-  if (list == NULL) return NULL;
+  //if (list == NULL) return NULL;
     
   Node * nodriz = (Node*) malloc (sizeof(Node));
   nodriz->next = list->head;

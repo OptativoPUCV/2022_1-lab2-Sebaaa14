294bdad9e8124fe560ab95c2db66aad01b6fad19
SEBASTIAN-ALES1 <>
2022-03-18 00:51:58 +0000

    new try

--- list.c
+++ list.c
@@ -43,11 +43,9 @@ void * firstList(List * list) {
 }
 
 void * nextList(List * list) {
-    if (list !=NULL || list->head !=NULL || list->current != NULL || list->current->next != NULL){
-      list->current = list->current->next;
-      return list->current;
-    }
-    else return NULL;
+    if (list ==NULL || list->head ==NULL || list->current == NULL || list->current->next == NULL) return NULL;
+  list->current = list->current->next;
+  return list->current->data;
 }
 
 void * lastList(List * list) {
